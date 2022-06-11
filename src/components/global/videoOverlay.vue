a35be2c2f7cc4a09e5ace2e3f962dc6aa04c7f60
drikishuk <benjaminaldrich9@gmail.com>
2022-06-11 22:32:48 +0100

    added modal initial

---
+++ src/components/global/videoOverlay.vue
@@ -0,0 +1,51 @@
+<template>
+  <aside v-if="modalOpen">
+    <button class="exit" @click="closeModal">X</button>
+    <video src="../../assets/banana.mp4" muted loop autoplay></video>
+  </aside>
+</template>
+
+<script>
+  export default {
+    props: {
+      modalOpen: {
+        type: Boolean,
+        required: true
+      }
+    },
+    methods: {
+      closeModal() {
+        this.$emit('closeModalEvent')
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  aside {
+    position: fixed;
+    top: 0;
+    background: black;
+    width: 100vw;
+    height: 100vh;
+    z-index: 10000;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+
+  video {
+    width: 60%;
+  }
+
+  .exit {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 10px 10px 0;
+    color: white;
+    background: none;
+    border: none;
+  }
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,9 @@
   <div>
     <about-header></about-header>
     <about-intro></about-intro> 
-    <about-row></about-row>  
-  
+    <about-row></about-row> 
+    <button @click="triggerModal">OPEN MODAL</button>
+    <video-overlay :modalOpen="jaidPoop" @closeModalEvent="triggerModal" />
   </div>
 </template>
 
@@ -11,13 +12,24 @@
   import aboutHeader from '@/components/about/aboutHeader';
   import aboutIntro from '@/components/about/aboutIntro';
   import aboutRow from '@/components/about/aboutRow';
- 
+  import videoOverlay from '@/components/global/videoOverlay'
 
   export default {
+    data() {
+      return {
+        jaidPoop: false
+      }
+    },
+    methods: {
+      triggerModal() {
+        this.jaidPoop = !this.jaidPoop;
+      },
+    },
     components: {
       aboutHeader,
       aboutIntro,
-      aboutRow 
+      aboutRow,
+      videoOverlay
     }
   }
 </script>
